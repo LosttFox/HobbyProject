a38cff25d4147282ffcf0964fe60e0e36d46a36f
LostFox <51424369+LosttFox@users.noreply.github.com>
2023-09-19 12:57:05 -0600

    renamed panel to basePanel

--- src/hobby/view/HobbyFrame.java
+++ src/hobby/view/HobbyFrame.java
@@ -9,19 +9,19 @@ public class HobbyFrame extends JFrame
 {
 	private HobbyController controller;
 	
-	private HobbyPanel panel;
+	private HobbyPanel basePanel;
 	
 	public HobbyFrame(HobbyController controller) 
 	{
 		this.controller = controller;
-		panel = new HobbyPanel(controller);
+		basePanel = new HobbyPanel(controller);
 	}
 	
 	private void setupFrame()
 	{
 		this.setTitle("Hobby Project");
 		this.setSize(500, 500);
-		this.setContentPane(panel);
+		this.setContentPane(basePanel);
 		this.setResizable(false);
 		this.setVisible(true);
 	}
